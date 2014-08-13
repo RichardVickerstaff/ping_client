46354ab98e02ccfcf2c05b980a179af775e463cc
Adam Whittingham <adam.whittingham@gmail.com>
2014-08-13 14:38:29 +0100

    Add shebang line for running directly

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install useful tools
+sudo apt-get install git vim curl htop
+
+# Install rvm
+curl -sSL https://get.rvm.io | bash -s stable
+
+# Push SSH key
+
+
+# clone client script
+git clone https://github.com/RichardVickerstaff/ping_client
